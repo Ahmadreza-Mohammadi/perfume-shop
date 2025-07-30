7e56c3830b6a10c1b8a5d20126d4994053ff27de
noway777 <ahmadrezamohammadi777@gmail.com>
2025-07-30 13:56:00 +0330

    feat: theme toggle added with logic

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,11 @@
+import HomeHeader from "@/components/home/HomeHeader";
+
 function page() {
-  return <div className="w-full flex justify-center">page</div>;
+  return (
+    <div className="m-auto max-w-[428px] bg-amber-300">
+      <HomeHeader />
+    </div>
+  );
 }
 
 export default page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import Menu from "@/components/menu/Menu";
-import "./globals.css";
+import "./globals.css"
+import type { ReactNode } from "react"
+import Menu from "@/components/menu/Menu"
+import ThemeProvider from "@/components/theme/ThemeProvider"
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}
-        <Menu />
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider>
+          {children}
+          <Menu />
+        </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
