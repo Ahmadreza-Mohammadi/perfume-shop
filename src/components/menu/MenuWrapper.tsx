7fce2d88601312a71449548efb77687b1c8eae40
noway777 <ahmadrezamohammadi777@gmail.com>
2025-08-13 14:37:42 +0330

    feat: add register page layout and UI

--- src/components/menu/MenuWrapper.tsx
+++ src/components/menu/MenuWrapper.tsx
@@ -4,6 +4,6 @@ import Menu from "./Menu";
 
 export default function MenuWrapper() {
   const pathname = usePathname();
-  if (pathname === "/login") return null;
+  if (pathname === "/login" || pathname === "/register") return null;
   return <Menu />;
 }
